19fe47ab5e466d242edc4adceca78022129dd97c
Paria <paria.slmni@gmail.com>
2024-05-24 18:32:11 +0330

    feat(page): landing page layout, style and assets 🚀

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    fontFamily: {
+      serif: 'Playfair Display',
+      sans: 'Poppins',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
